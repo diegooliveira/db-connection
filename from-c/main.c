652ccb26059515e3a2eeeec7034320125dc188be
Diego de Oliveira <diego@diegooliveira.com>
2012-06-28 10:53:02 -0400

    conectando a partir do Java
    Classe de conexão e consulta no banco de dados e biblioteca de dependencia.

--- from-c/main.c
+++ from-c/main.c
@@ -1,35 +1,43 @@
 #include <mysql.h>
 #include <stdio.h>
+
 int main() {
 
-   MYSQL *conn;
-   MYSQL_RES *res;
-   MYSQL_ROW row;
+    // Referencia para a conexão
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
 
-   char *server = "localhost";
-   char *user = "root";
-   char *password = "root"; /* set me first */
-   char *database = "mysql";
-   conn = mysql_init(NULL);
+    char *server = "localhost";
+    char *user = "root";
+    char *password = "root"; /* set me first */
+    char *database = "mysql";
+    conn = mysql_init(NULL);
 
-   /* Connect to database */
-   if (!mysql_real_connect(conn, server,
-         user, password, database, 0, NULL, 0)) {
+   // Abre a conexão com o banco de dados, se não tiver sucesso 
+   // entra no if, imprime a mensagem de erro e sai.
+   printf("--> Conectando no banco de dados\n");
+   if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)) {
       fprintf(stderr, "%s\n", mysql_error(conn));
-      exit(1);
+      return 1;
    }
 
-   /* send SQL query */
-   if (mysql_query(conn, "show tables")) {
-      fprintf(stderr, "%s\n", mysql_error(conn));
-      exit(1);
-   }
-   res = mysql_use_result(conn);
-   /* output table name */
-   printf("MySQL Tables in mysql database:\n");
-   while ((row = mysql_fetch_row(res)) != NULL)
-      printf("%s \n", row[0]);
-   /* close connection */
-   mysql_free_result(res);
-   mysql_close(conn);
+    char *query = "show tables;";
+    printf("->> Executando consulta: [ %s ]\n", query);
+    if (mysql_query(conn, query)) {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        return 1;
+    }
+    res = mysql_use_result(conn);
+    
+    /* output table name */
+    printf("->> Imprimindo resultado.\n");
+    while ((row = mysql_fetch_row(res)) != NULL)
+        printf("%s \n", row[0]);
+   
+    /* close connection */
+    mysql_free_result(res);
+   
+    printf("->> Desconectando do banco de dados.\n");
+    mysql_close(conn);
 }
---
+++ from-java/ConectarMySql.java
@@ -0,0 +1,60 @@
+
+import java.sql.*;
+
+public class ConectarMySql {
+
+    public static void main(String args[]){
+        System.out.println("MySQL Connect Example.");
+
+        // Nome do banco de dados.
+        String nomeDoBanco = "mysql";
+
+        // Cada banco de dados tem sua forma particular de formar a url de
+        // conexão, por padrão tem a forma:
+        //      jdbc:[driver]:[informações do driver]
+        // Procure pela forma de conexão para outro bancos de dados
+        String url = "jdbc:mysql://localhost:3306/" + nomeDoBanco;
+        
+        // Cada banco de dados tem seu conector, esse é o do MySql
+        String driver = "com.mysql.jdbc.Driver";
+        
+        // Usuário e senha do banco de dados
+        String nomeDoUsuario = "root"; 
+        String senha = "root";
+        
+        // Objeto que guarda a conexão
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            // Registrando o driver.
+            Class.forName(driver).newInstance();
+            
+            // Abrindo a conexão, se tiver falha pula para o tratamento de
+            // error
+            System.out.println("->> Conectando no banco de dados [ " + url + " ]");
+            conn = DriverManager.getConnection(url, nomeDoUsuario, senha);
+
+            String query = "show tables;";
+            System.out.println("->> Executando consulta: [ " + query + " ]");
+            st = conn.createStatement();
+            rs = st.executeQuery(query);
+            
+            System.out.println("->> Imprimindo resultado.");
+            while( rs.next() ){
+                System.out.println( rs.getString(1) );
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                System.out.println("->> Desconectando do banco de dados.");
+                if (conn != null)
+                    conn.close();
+            } catch (Exception ex){
+            }
+        }
+    }
+
+}
